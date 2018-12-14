4816d093f139dd0728cff3295256be4331010b23
shiminghua <shiminghua2008@qq.com>
2018-12-14 11:23:50 +0800

    fixed bug

--- samsung/2018slqs/js/index.js
+++ samsung/2018slqs/js/index.js
@@ -37,11 +37,11 @@
  * @return undefined  无返回值
  */
 function imagesIsAllLoaded(imgArr, callback) {
-    let imgs = [];
-    let len = imgArr.length;
-    let flag = 0;
+    var imgs = [];
+    var len = imgArr.length;
+    var flag = 0;
 
-    for (let i = 0; i < len; i++) {
+    for (var i = 0; i < len; i++) {
         imgs[i] = new Image();
         imgs[i].onload = function () {
             flag++;
@@ -54,126 +54,197 @@ function imagesIsAllLoaded(imgArr, callback) {
 }
 ~function () {
 
-    let imgArr = [];
+    var imgArr = [];
     $('img').each((index, image) => {
         imgArr.push(image.src);
     });
 
     imgArr.push('images/bg.jpg', 'images/bg2.jpg');
     imagesIsAllLoaded(imgArr, (imgs) => {
-        $("#loading").css("display", "none");
+        $("#loading").hide();
         First();
     });
 
 }();
 function First() {
-    $("#Home>.header>.title>img").addClass("animated bounceInLeft");
-    $("#Home>.header>.logo>img").addClass("animated bounceInRight");
-    $("#arrow").css({ "left": "0", "top": "27%", "right": "0", "bottom": "0", "margin": "auto" })
+    $("#home-title").addClass("animated bounceInLeft");
+    $("#home-logo").addClass("animated bounceInRight");
+
     setTimeout(function () {
         $("#Home>.center").css("display", "block");
         $("#Home>.center>img").eq(0).addClass("animated zoomInDown");
         $("#Home>.center>img").eq(1).addClass("animated flip");
     }, 800);
 
+    $("#arrow").css({ "top": "62%" }).show();
 }
 
-$("#Home").on("click", function () {
-    this.style.display = "none";
-    $("#arrow").css({ "display": "none", "left": "50%", "top": "97%", "margin": "unset" })
-    var myTest = $('#maseger'), pItems = myTest.find('.B');
-    pItems.eq(0).animate({ height: "3rem" }, 5000, function () {
-        $("#start").animate({ opacity: 1 }, 600);
-        $("#button").on("click", function () {
-            $("#box").css("display", "none")
-            $("#Article").css("display", "none");
-            $("#start").css("display", "none");
-            $("#maseger").css({ "background-image": "url(./images/bg3.jpg)" });
-            $("#Answer").css("display", "block");
-            var listLength = $("#Answer li").length;
-            for (let i = 0; i < listLength; i++) {
-                setInterval(() => {
-                    $("#Answer li").eq(i).addClass("animated bounceInRight").css("display", "block")
-                }, i * 300)
-            }
-        })
+function showAnswers() {
+    var $home = $('#Home');
+    var $arrow = $('#arrow');
+    var $articleBox = $('#article-box');
+    var $start = $("#start");
+    var $answer = $('#Answer');
+
+    $home.on('click', function () {
+        $home.hide();
+        $arrow.hide();
+        $articleBox.show();
+
+        $('#article-ul').animate({ height: '3rem' }, 5000, function () {
+            $start.animate({ opacity: 1 }, 600);
+        });
+
     });
 
-});
-$("#arrow").on("click", function () {
-    $("#Home").css("display", "none");
-    this.style.display = "none";
-    var myTest = $('#maseger'), pItems = myTest.find('.B');
-    pItems.eq(0).animate({ height: "3rem" }, 2500, function () {
-        $("#start").animate({ opacity: 1 }, 600);
-        $("#button").on("click", function () {
-            $("#box").css("display", "none")
-            $("#Article").css("display", "none");
-            $("#start").css("display", "none");
-            $("#maseger").css({ "background-image": "url(./images/bg3.jpg)" });
-            $("#Answer").css("display", "block");
-            var listLength = $("#Answer li").length;
-            console.length(listLength)
-            for (let i = 0; i < listLength; i++) {
-                var item = listLength[i];
-                setInterval(() => {
-                    $("#Answer li").eq(i).addClass("animated bounceInRight").css("display", "block")
-                }, i * 300)
-                debugger
-            }
-        })
+    $start.on('click', function () {
+        $articleBox.hide();
+        $answer.show();
+        $answer.find('li').each(function (index, item) {
+            setInterval(function () {
+                $(item).addClass("animated bounceInRight").css("display", "block")
+            }, index * 300)
+        });
     });
-})
-$("#Answer li").on("click", function () {
-    $("#Answer").css({ "display": "none" });
-    $("#TopicTwo").css({ "display": "block" })
-    var listLength = $("#TopicTwo li").length;
-    for (let i = 0; i < listLength; i++) {
-        setInterval(() => {
-            $("#TopicTwo li").eq(i).addClass("animated bounceInRight").css("display", "block")
-        }, i * 300)
-    }
+
+}
+
+function answersAnimate() {
+    var $answer = $('#Answer');
+    var $TopicTwo = $('#TopicTwo');
+    var $TopicSan = $('#TopicSan');
+    var $TopicSi = $('#TopicSi');
+    var $TopicFive = $('#TopicFive');
+
+    $answer.find('li').on('click', function () {
+        $answer.hide();
+        $TopicTwo.show();
+        $TopicTwo.find('li').each(function (index, item) {
+            setInterval(function () {
+                $(item).addClass("animated bounceInRight").css("display", "block")
+            }, index * 300)
+        });
+    });
+
+    $TopicTwo.find('li').on('click', function () {
+        $TopicTwo.hide();
+        $TopicSan.show();
+        $TopicSan.find('li').each(function (index, item) {
+            setInterval(function () {
+                $(item).addClass("animated bounceInRight").css("display", "block")
+            }, index * 300)
+        });
+    });
+
+    $TopicSan.find('li').on('click', function () {
+        $TopicSan.hide();
+        $TopicSi.show();
+        $TopicSi.find('li').each(function (index, item) {
+            setInterval(function () {
+                $(item).addClass("animated bounceInRight").css("display", "block")
+            }, index * 300)
+        });
+    });
+
+    $TopicSi.find('li').on('click', function () {
+        $TopicSi.hide();
+        $TopicFive.show();
+        $TopicFive.find('li').each(function (index, item) {
+            setInterval(function () {
+                $(item).addClass("animated bounceInRight").css("display", "block")
+            }, index * 300)
+        });
+    });
+
+    $TopicFive.find('li').on('click', function () {
+        $TopicFive.hide();
+        // $('#Result5').show();
+    });
+}
+
+$(document).ready(function () {
+    showAnswers();
+    answersAnimate();
+    initWxSdk();
 });
 
-$("#TopicTwo li").on("click", function () {
-    $("#TopicTwo").css({ "display": "none" });
-    $("#TopicSan").css({ "display": "block" })
-    var listLength = $("#TopicSan li").length;
-    for (let i = 0; i < listLength; i++) {
-        setInterval(() => {
-            $("#TopicSan li").eq(i).addClass("animated bounceInRight").css("display", "block")
-        }, i * 300)
-    }
-});
 
-$("#TopicSan li").on("click", function () {
-    $("#TopicSan").css({ "display": "none" });
-    $("#TopicSi").css({ "display": "block" })
-    var listLength = $("#TopicSan li").length;
-    for (let i = 0; i < listLength; i++) {
-        setInterval(() => {
-            $("#TopicSi li").eq(i).addClass("animated bounceInRight").css("display", "block")
-        }, i * 300)
-    }
-});
 
-$("#TopicSi li").on("click", function () {
-    $("#TopicSi").css({ "display": "none" });
-    $("#TopicFive").css({ "display": "block" })
-    var listLength = $("#TopicFive li").length;
-    for (let i = 0; i < listLength; i++) {
-        setInterval(() => {
-            $("#TopicFive li").eq(i).addClass("animated bounceInRight").css("display", "block")
-        }, i * 300)
-    }
-});
 
-$("#TopicFive li").on("click", function () {
-    $("#TopicSi").css({ "display": "none" });
-    $("#TopicFive").css({ "display": "none" })
-    // $(".Result").css({ "display": "block" })
+// $("#arrow").on("click", function () {
+//     $("#Home").css("display", "none");
+//     this.style.display = "none";
+//     var myTest = $('#maseger'), pItems = myTest.find('.B');
+//     pItems.eq(0).animate({ height: "3rem" }, 2500, function () {
+//         $("#start").animate({ opacity: 1 }, 600);
+//         $("#button").on("click", function () {
+//             $("#box").css("display", "none")
+//             $("#Article").css("display", "none");
+//             $("#start").css("display", "none");
+//             $("#maseger").css({ "background-image": "url(./images/bg3.jpg)" });
+//             $("#Answer").css("display", "block");
+//             var listLength = $("#Answer li").length;
+//             console.length(listLength)
+//             for (var i = 0; i < listLength; i++) {
+//                 var item = listLength[i];
+//                 setInterval(function () {
+//                     $("#Answer li").eq(i).addClass("animated bounceInRight").css("display", "block")
+//                 }, i * 300)
+//                 // debugger
+//             }
+//         })
+//     });
+// })
+// $("#Answer li").on("click", function () {
+//     $("#Answer").css({ "display": "none" });
+//     $("#TopicTwo").css({ "display": "block" })
+//     var listLength = $("#TopicTwo li").length;
+//     for (var i = 0; i < listLength; i++) {
+//         setInterval(function () {
+//             $("#TopicTwo li").eq(i).addClass("animated bounceInRight").css("display", "block")
+//         }, i * 300)
+//     }
+// });
 
-});
+// $("#TopicTwo li").on("click", function () {
+//     $("#TopicTwo").css({ "display": "none" });
+//     $("#TopicSan").css({ "display": "block" })
+//     var listLength = $("#TopicSan li").length;
+//     for (var i = 0; i < listLength; i++) {
+//         setInterval(function () {
+//             $("#TopicSan li").eq(i).addClass("animated bounceInRight").css("display", "block")
+//         }, i * 300)
+//     }
+// });
+
+// $("#TopicSan li").on("click", function () {
+//     $("#TopicSan").css({ "display": "none" });
+//     $("#TopicSi").css({ "display": "block" })
+//     var listLength = $("#TopicSan li").length;
+//     for (var i = 0; i < listLength; i++) {
+//         setInterval(function () {
+//             $("#TopicSi li").eq(i).addClass("animated bounceInRight").css("display", "block")
+//         }, i * 300)
+//     }
+// });
+
+// $("#TopicSi li").on("click", function () {
+//     $("#TopicSi").css({ "display": "none" });
+//     $("#TopicFive").css({ "display": "block" })
+//     var listLength = $("#TopicFive li").length;
+//     for (var i = 0; i < listLength; i++) {
+//         setInterval(function () {
+//             $("#TopicFive li").eq(i).addClass("animated bounceInRight").css("display", "block")
+//         }, i * 300)
+//     }
+// });
+
+// $("#TopicFive li").on("click", function () {
+//     $("#TopicSi").css({ "display": "none" });
+//     $("#TopicFive").css({ "display": "none" })
+//     // $(".Result").css({ "display": "block" })
+
+// });
 
 
 
@@ -190,14 +261,14 @@ touch.on(".screenshot", "tap", function () {
 
 
 function chooseTopic(callback) {
-    let selected = {
+    var selected = {
         a: 0,
         b: 0,
         c: 0,
         d: 0,
         e: 0,
     };
-    let results = {
+    var results = {
         a: '#Result1',
         b: '#Result2',
         c: '#Result3',
@@ -205,20 +276,20 @@ function chooseTopic(callback) {
         e: '#Result5',
     };
 
-    let liss = $('#Answer li, #TopicTwo li, #TopicSan li, #TopicSi li, #TopicFive li');
+    var liss = $('#Answer li, #TopicTwo li, #TopicSan li, #TopicSi li, #TopicFive li');
 
     liss.bind('click', (e) => {
-        let node = $(e.target);
+        var node = $(e.target);
         if (node[0].nodeName === 'SPAN') {
             node = node.parent();
         }
-        let checkedValue = node.attr('data-value');
+        var checkedValue = node.attr('data-value');
         selected[checkedValue]++;
     });
 
     // 获取最大的数字
     function getMaxNumber(anwsers, results) {
-        let result = 0;
+        var result = 0;
         Object.values(anwsers).forEach((value) => {
             if (value > result) {
                 result = value;
@@ -236,26 +307,26 @@ function chooseTopic(callback) {
 
     // 获取结果
     function getResult(ans) {
-        let result, len = ans.length;
+        var result, len = ans.length;
         if (len <= 1) {
             result = ans[0];
         }
         else {
-            let random = Math.floor(Math.random() * len);
+            var random = Math.floor(Math.random() * len);
             result = ans[random];
         }
         return results[result];
     }
 
     function getMyResult() {
-        let num = getMaxNumber(selected);
-        let ans = getAnwsers(selected, num);
-        let result = getResult(ans);
+        var num = getMaxNumber(selected);
+        var ans = getAnwsers(selected, num);
+        var result = getResult(ans);
         return result;
     }
 
-    $('#TopicFive').bind('click', () => {
-        let result = getMyResult();
+    $('#TopicFive').bind('click', function () {
+        var result = getMyResult();
         callback && callback(result, selected);
     });
 
@@ -266,35 +337,35 @@ chooseTopic(function (id, selected) {
     console.log('----', id, selected);
     $(id).show();
     $(id + '-img').show();
+    $("#arrow").css({ "top": "61%", "left": "79%" }).show();
     $("#maseger").css({ "background-image": "url(./images/note5/bg4.jpg)" });
     if (id == "#Result1") {
         $("#Result1>.Preservation").css({ "width": "100%", "right": "0" })
-        $("#arrow").css({ "top": "63%", "left": "73%" })
         $("#Result1>.super").css("display", "block")
     }
 
-    let animate1 = $(id + ">.Template>img");
-    let animateArr = ["animated bounceInLeft", "animated bounceInRight", "animated fadeInDown"];
-    for (let i = 0, len = animate1.length; i < len; i++) {
-        setInterval(() => {
+    var animate1 = $(id + ">.Template>img");
+    var animateArr = ["animated bounceInLeft", "animated bounceInRight", "animated fadeInDown"];
+    for (var i = 0, len = animate1.length; i < len; i++) {
+        setInterval(function () {
             animate1.eq(i).addClass(animateArr[i]).css("display", "block");
         }, i * 500);
     }
     var LeftLength = $(id + ">.aperture>.Left>img").length;
-    for (let i = 0; i < LeftLength; i++) {
-        setInterval(() => {
+    for (var i = 0; i < LeftLength; i++) {
+        setInterval(function () {
             $(id + ">.aperture>.Left>img").eq(0).addClass("animated rotateInDownLeft").css("display", "block");
             $(id + ">.aperture>.Left>img").eq(1).addClass("animated rotateInDownRight").css("display", "block");
         }, i * 500)
     }
     var RightLength = $(id + ">.aperture>.Right>img").length;
-    for (let i = 0; i < RightLength; i++) {
-        setInterval(() => {
+    for (var i = 0; i < RightLength; i++) {
+        setInterval(function () {
             $(id + ">.aperture>.Right>img").eq(0).addClass("animated flip").css("display", "block");
             $(id + ">.aperture>.Right>img").eq(1).addClass("animated rotateInUpRight").css("display", "block");
         }, i * 500)
     }
-    setInterval(() => {
+    setInterval(function () {
         $(".Result span").css("display", "block");
     }, 1000)
 });
@@ -303,7 +374,7 @@ function initWxSdk() {
     var shareTitle = "寻找属于你的电竞星系";
     var shareUrl = window.location.href;
     var shareDesc = "你是哪一种电竞星系？";
-    var shareImg = "http://www.h5app.com.cn/samsung/story1/share.png";
+    var shareImg = "http://www.h5app.com.cn/samsung/xlqs2018/images/1521544753295_.pic_hd.jpg";
     $.ajax({
         type: 'GET', //GET
         url: "//www.h5app.com.cn/wxapi/getInitInfo.php?reqUrl=" + encodeURIComponent(window.location.href),
@@ -392,4 +463,3 @@ function initWxSdk() {
     });
 }
 
-initWxSdk();
